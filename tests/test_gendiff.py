e5a3973a2c9ce73a34daea79211c6d5154ae83bd
Evgeny <fill1986@gmail.com>
2022-03-22 20:39:03 +0300

    add Pathlib in testcoverage

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,11 +1,11 @@
-import argparse
+import pathlib
 import json
 from gendiff_package.scripts.gendiff import generate_diff
 
-
-file_1 = json.load(open('/home/evgeny/python-project-lvl2/tests/fixtures/file1.json'))
-file_2 = json.load(open('/home/evgeny/python-project-lvl2/tests/fixtures/file2.json'))
-result_json = open('/home/evgeny/python-project-lvl2/tests/fixtures/result.txt')
+dir_path = pathlib.Path.cwd()
+file_1 = json.load(open(pathlib.Path(dir_path, 'tests', 'fixtures', 'file1.json')))
+file_2 = json.load(open(pathlib.Path(dir_path, 'tests', 'fixtures', 'file2.json')))
+result_json = open(pathlib.Path(dir_path, 'tests', 'fixtures', 'result.txt'))
 
 
 def test_json():
